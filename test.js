5628e6480c771423bf50c3b4119c66cb0536ca2e
Amber Brownstein <amberbrownstein@gmail.com>
2025-04-04 19:07:56 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,14 +2,11 @@ let S = require("./scheduler.js");
 let CS = require("./checkSchedule.js");
 async function test() {
     const ret = await CS.checkSchedule(new Date(), 7, [188559, 41738]);
-    //ret.forEach((ret) => console.log(`\nReturned: ${JSON.stringify(ret)}`));
-    return ret;
+    ret.forEach((ret) => console.log(`\nReturned: ${JSON.stringify(ret)}`));
 }
 async function testFileStuff() {
     const newData = await CS.checkSchedule(new Date(), 7, [188559, 41738]);
     const sendEmail = S.updateFileData('currentAvailability.json', newData);
-    //console.log(sendEmail);
 }
 
 testFileStuff();
-//test();
